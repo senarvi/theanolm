f34ead40a44be5242a64bf81fced57e3f81180fd
Seppo Enarvi <seppo.git@marjaniemi.com>
2015-11-23 13:49:08 +0200

    Normalize cost / gradients by the actual number of training examples, not by the maximum number of words in the mini-batch.

--- theanolm/optimizers/basicoptimizer.py
+++ theanolm/optimizers/basicoptimizer.py
@@ -45,9 +45,9 @@ class BasicOptimizer(object):
         # predicted) is masked out.
         logprobs = logprobs * self.network.minibatch_mask[1:]
         # Calculate the negative log probability normalized by the number of
-        # training examples in the mini-batch. By taking a mean instead of a
-        # sum, the gradients will also be normalized by the number of words.
-        cost = -logprobs.mean()
+        # training examples in the mini-batch, so that the gradients will also
+        # be normalized by the number of training examples.
+        cost = -logprobs.sum() / self.network.minibatch_mask[1:].sum()
 
         # Compute the symbolic expression for updating the gradient with regard
         # to each parameter.
