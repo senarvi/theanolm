87633a03934bd88c4d188b2c89544dd7f07afb68
Seppo Enarvi <seppo.git@marjaniemi.com>
2016-03-09 20:12:35 +0200

    Optimizer._move() moves a word to another class.

--- wordclasses/optimizer.py
+++ wordclasses/optimizer.py
@@ -5,13 +5,14 @@ import numpy
 
 class Optimizer(object):
     '''
-    classdocs
+    Clusters words into classes.
     '''
 
     def __init__(self, num_classes, corpus_file, vocabulary_file = None):
         '''
-        Reads the vocabulary from the corpus, which may be restricted by
-        `vocabulary_file`. Then reads the statistics from the corpus.
+        Reads the vocabulary from the text ``corpus_file``. The vocabulary may
+        be restricted by ``vocabulary_file``. Then reads the statistics from the
+        text.
         '''
 
         # Read the vocabulary.
@@ -29,6 +30,7 @@ class Optimizer(object):
         # Create word IDs and read word statistics.
         self.vocabulary_size = len(self.vocabulary)
         self.word_ids = dict(zip(self.vocabulary, range(self.vocabulary_size)))
+        corpus_file.seek(0)
         self._read_word_statistics(corpus_file)
 
         # Initialize classes and compute class statistics.
@@ -41,7 +43,7 @@ class Optimizer(object):
         '''
 
         self.word_counts = numpy.zeros(self.vocabulary_size, numpy.int64)
-        self.word_bigram_counts = numpy.zeros(
+        self.word_word_counts = numpy.zeros(
             (self.vocabulary_size, self.vocabulary_size), numpy.int64)
 
         for line in corpus_file:
@@ -51,10 +53,11 @@ class Optimizer(object):
                     sentence.append(self.word_ids[word])
                 else:
                     sentence.append(self.word_ids['<UNK>'])
+            sentence.append(self.word_ids['</s>'])
             for word_id in sentence:
                 self.word_counts[word_id] += 1
-            for left, right in zip(sentence[:-1], sentence[1:]):
-                self.word_bigram_counts[left, right] += 1
+            for left_word_id, right_word_id in zip(sentence[:-1], sentence[1:]):
+                self.word_word_counts[left_word_id, right_word_id] += 1
 
     def _random_init_classes(self, num_classes):
         '''
@@ -74,7 +77,6 @@ class Optimizer(object):
         self.class_to_words[2].add(self.word_ids['<UNK>'])
 
         class_id = 3
-        print(self.word_to_class)
         for word_id, _ in sorted(enumerate(self.word_counts),
                                  key=lambda x: x[1]):
             if not self.word_to_class[word_id] is None:
@@ -90,9 +92,66 @@ class Optimizer(object):
         '''
 
         self.class_counts = numpy.zeros(self.num_classes, numpy.int64)
-        self.class_bigram_counts = numpy.zeros(
+        self.class_class_counts = numpy.zeros(
             (self.num_classes, self.num_classes), numpy.int64)
-        self.class_word_counts = numpy.zeros(self.vocabulary_size, numpy.int64)
-        self.word_class_counts = numpy.zeros(self.vocabulary_size, numpy.int64)
+        self.class_word_counts = numpy.zeros(
+            (self.num_classes, self.vocabulary_size), numpy.int64)
+        self.word_class_counts = numpy.zeros(
+            (self.vocabulary_size, self.num_classes), numpy.int64)
 
+        for word_id, class_id in enumerate(self.word_to_class):
+            self.class_counts[class_id] += self.word_counts[word_id]
+        for (left_word_id, right_word_id), count in numpy.ndenumerate(self.word_word_counts):
+            left_class_id = self.word_to_class[left_word_id]
+            right_class_id = self.word_to_class[right_word_id]
+            self.class_class_counts[left_class_id,right_class_id] += count
+            self.class_word_counts[left_class_id,right_word_id] += count
+            self.word_class_counts[left_word_id,right_class_id] += count
+
+    def _evaluate_move(self, word_id, new_class_id):
+        '''
+        Evaluates how much moving a word to another class would change the log
+        likelihood.
+        '''
+
+        pass
+
+    def _move(self, word_id, new_class_id):
+        '''
+        Moves a word to another class.
+        '''
+
+        old_class_id = self.word_to_class[word_id]
+        word_count = self.word_counts[word_id]
+        self.class_counts[old_class_id] -= word_count
+        self.class_counts[new_class_id] += word_count
         
+        for right_word_id, count in enumerate(self.word_word_counts[word_id,:]):
+            if right_word_id == word_id:
+                continue
+            right_class_id = self.word_to_class[right_word_id]
+            self.class_class_counts[old_class_id,right_class_id] -= count
+            self.class_class_counts[new_class_id,right_class_id] += count
+            self.class_word_counts[old_class_id,right_word_id] -= count
+            self.class_word_counts[new_class_id,right_word_id] += count
+        
+        for left_word_id, count in enumerate(self.word_word_counts[:,word_id]):
+            if left_word_id == word_id:
+                continue
+            left_class_id = self.word_to_class[left_word_id]
+            self.class_class_counts[left_class_id,old_class_id] -= count
+            self.class_class_counts[left_class_id,new_class_id] += count
+            self.word_class_counts[left_word_id,old_class_id] -= count
+            self.word_class_counts[left_word_id,new_class_id] += count
+
+        count = self.word_word_counts[word_id,word_id]
+        self.class_class_counts[old_class_id,old_class_id] -= count
+        self.class_class_counts[new_class_id,new_class_id] += count
+        self.class_word_counts[old_class_id,word_id] -= count
+        self.class_word_counts[new_class_id,word_id] += count
+        self.word_class_counts[word_id,old_class_id] -= count
+        self.word_class_counts[word_id,new_class_id] += count
+
+        self.class_to_words[old_class_id].remove(word_id)
+        self.class_to_words[new_class_id].add(word_id)
+        self.word_to_class[word_id] = new_class_id
--- wordclasses/tests/testoptimizer.py
+++ wordclasses/tests/testoptimizer.py
@@ -3,6 +3,7 @@
 
 import unittest
 import os
+import numpy
 from wordclasses import Optimizer
 
 class TestOptimizer(unittest.TestCase):
@@ -22,12 +23,80 @@ class TestOptimizer(unittest.TestCase):
         self.assertEqual(optimizer.num_classes, num_classes + 3)
         self.assertEqual(len(optimizer.word_to_class), num_words)
         self.assertEqual(len(optimizer.class_to_words), num_classes + 3)
+
         self.assertEqual(len(optimizer.word_counts), num_words)
-        self.assertEqual(optimizer.word_bigram_counts.shape[0], num_words)
-        self.assertEqual(optimizer.word_bigram_counts.shape[1], num_words)
+        self.assertEqual(optimizer.word_counts[optimizer.word_ids['<s>']], 11)
+        self.assertEqual(optimizer.word_counts[optimizer.word_ids['a']], 13)
+        self.assertEqual(optimizer.word_counts[optimizer.word_ids['b']], 8)
+        self.assertEqual(optimizer.word_counts[optimizer.word_ids['c']], 8)
+        self.assertEqual(optimizer.word_counts[optimizer.word_ids['d']], 11)
+        self.assertEqual(optimizer.word_counts[optimizer.word_ids['e']], 15)
+        self.assertEqual(optimizer.word_counts[optimizer.word_ids['<UNK>']], 0)
+        self.assertEqual(optimizer.word_counts[optimizer.word_ids['</s>']], 11)
+
+        self.assertEqual(optimizer.word_word_counts.shape[0], num_words)
+        self.assertEqual(optimizer.word_word_counts.shape[1], num_words)
         self.assertEqual(len(optimizer.class_counts), num_classes + 3)
-        self.assertEqual(optimizer.class_bigram_counts.shape[0], num_classes + 3)
-        self.assertEqual(optimizer.class_bigram_counts.shape[1], num_classes + 3)
+        self.assertEqual(optimizer.class_class_counts.shape[0], num_classes + 3)
+        self.assertEqual(optimizer.class_class_counts.shape[1], num_classes + 3)
+
+        self.assertEqual(optimizer.class_word_counts.shape[0], num_classes + 3)
+        self.assertEqual(optimizer.class_word_counts.shape[1], num_words)
+        self.assertEqual(optimizer.word_class_counts.shape[0], num_words)
+        self.assertEqual(optimizer.word_class_counts.shape[1], num_classes + 3)
+
+    def test_move_and_back(self):
+        num_classes = 2
+        optimizer = Optimizer(num_classes, self.sentences_file)
+
+        orig_class_counts = numpy.copy(optimizer.class_counts)
+        orig_class_class_counts = numpy.copy(optimizer.class_class_counts)
+        orig_class_word_counts = numpy.copy(optimizer.class_word_counts)
+        orig_word_class_counts = numpy.copy(optimizer.word_class_counts)
+
+        word_id = optimizer.word_ids['d']
+        orig_class_id = optimizer.word_to_class[word_id]
+        new_class_id = 3 if orig_class_id != 3 else 4
+        optimizer._move(word_id, new_class_id)
+
+        self.assertEqual(numpy.count_nonzero(optimizer.class_counts != orig_class_counts), 2)
+        self.assertEqual(numpy.sum(optimizer.class_counts), numpy.sum(orig_class_counts))
+        self.assertGreater(numpy.count_nonzero(optimizer.class_class_counts != orig_class_class_counts), 0)
+        self.assertEqual(numpy.sum(optimizer.class_class_counts), numpy.sum(orig_class_class_counts))
+        self.assertGreater(numpy.count_nonzero(optimizer.class_word_counts != orig_class_word_counts), 0)
+        self.assertEqual(numpy.sum(optimizer.class_word_counts), numpy.sum(orig_class_word_counts))
+        self.assertGreater(numpy.count_nonzero(optimizer.word_class_counts != orig_word_class_counts), 0)
+        self.assertEqual(numpy.sum(optimizer.word_class_counts), numpy.sum(orig_word_class_counts))
+
+        optimizer._move(word_id, orig_class_id)
+
+        self.assertEqual(numpy.count_nonzero(optimizer.class_counts != orig_class_counts), 0)
+        self.assertEqual(numpy.count_nonzero(optimizer.class_class_counts != orig_class_class_counts), 0)
+        self.assertEqual(numpy.count_nonzero(optimizer.class_word_counts != orig_class_word_counts), 0)
+        self.assertEqual(numpy.count_nonzero(optimizer.word_class_counts != orig_word_class_counts), 0)
+
+    def test_move_and_recompute(self):
+        num_classes = 2
+
+        optimizer1 = Optimizer(num_classes, self.sentences_file)
+        word_id = optimizer1.word_ids['d']
+        orig_class_id = optimizer1.word_to_class[word_id]
+        new_class_id = 3 if orig_class_id != 3 else 4
+        optimizer1.class_to_words[orig_class_id].remove(word_id)
+        optimizer1.class_to_words[new_class_id].add(word_id)
+        optimizer1.word_to_class[word_id] = new_class_id
+        optimizer1._compute_class_statistics()
+
+        self.sentences_file.seek(0)
+        optimizer2 = Optimizer(num_classes, self.sentences_file)
+        word_id = optimizer2.word_ids['d']
+        orig_class_id = optimizer2.word_to_class[word_id]
+        optimizer2._move(word_id, new_class_id)
+
+        self.assertEqual(numpy.count_nonzero(optimizer1.class_counts != optimizer2.class_counts), 0)
+        self.assertEqual(numpy.count_nonzero(optimizer1.class_class_counts != optimizer2.class_class_counts), 0)
+        self.assertEqual(numpy.count_nonzero(optimizer1.class_word_counts != optimizer2.class_word_counts), 0)
+        self.assertEqual(numpy.count_nonzero(optimizer1.word_class_counts != optimizer2.word_class_counts), 0)
 
 if __name__ == '__main__':
     unittest.main()
