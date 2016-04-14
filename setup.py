e5b5e0eaf2dedf866fbfa517cb752060f3b266e4
Seppo Enarvi <seppo.git@marjaniemi.com>
2016-04-14 18:45:09 +0300

    Setuptools distribution.

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+from glob import glob
+from setuptools import setup
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+version = subprocess.check_output(['git', 'describe'], cwd=script_dir)
+version = version.decode('utf-8').rstrip()
+scripts = glob(os.path.join(script_dir, 'bin', '*'))
+architectures = glob(os.path.join(script_dir, 'architectures', '*.arch'))
+
+long_description = 'TheanoLM is a recurrent neural network language modeling ' \
+                   'toolkit implemented using Theano. Theano allows the user ' \
+                   'to customize and extend the neural network very ' \
+                   'conveniently, still generating highly efficient code ' \
+                   'that can utilize multiple GPUs or CPUs for parallel ' \
+                   'computation. TheanoLM allows the user to specify ' \
+                   'arbitrary network architecture. New layer types and ' \
+                   'optimization methods can be easily implemented.'
+keywords = 'theano neural network language modeling machine learning research'
+classifiers = ['Development Status :: 4 - Beta',
+               'Environment :: Console',
+               'Programming Language :: Python :: 3',
+               'Intended Audience :: Science/Research',
+               'License :: OSI Approved :: Apache Software License',
+               'Operating System :: OS Independent',
+               'Topic :: Scientific/Engineering']
+
+setup(name='TheanoLM',
+      version=version,
+      author='Seppo Enarvi',
+      author_email='seppo2016@marjaniemi.com',
+      url='https://github.com/senarvi/theanolm',
+      description='Toolkit for neural network language modeling using Theano',
+      long_description=long_description,
+      license='Apache License, Version 2.0',
+      keywords=keywords,
+      classifiers=classifiers,
+      packages=['theanolm', 'theanolm.commands', 'theanolm.iterators',
+                'theanolm.layers', 'theanolm.optimizers', 'theanolm.stoppers',
+                'theanolm.trainers', 'wordclasses'],
+      scripts=scripts,
+      data_files=[('architectures', architectures)],
+      install_requires=['numpy', 'scipy', 'theano', 'h5py'])
