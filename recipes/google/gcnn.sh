05ef33712c3c79923fae0deefa28a93982e1828c
Seppo Enarvi <seppo.git@marjaniemi.com>
2017-10-20 14:30:12 +0300

    Attempted training on one training file with a faster architecture.

---
+++ recipes/google/gcnn.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -e
+#SBATCH --partition gpushort
+#SBATCH --time=4:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --mem=48G
+
+#
+# Examples for training TheanoLM models on the 1 Billion Word Language Model
+# Benchmark. The vocabulary is limited to words that appear at least three times
+# and <unk> is used to denote the other words.
+#
+
+if [ -z "${SLURM_SUBMIT_DIR}" ]
+then
+	script_dir=$(dirname "${0}")
+	script_dir=$(readlink -e "${script_dir}")
+else
+	script_dir="${SLURM_SUBMIT_DIR}"
+fi
+arch_dir="${script_dir}/../architectures"
+
+# Load paths to the corpus files. You need to create paths.sh with:
+#
+# TRAIN_FILES=(/path/to/google/1-billion-word-language-modeling-benchmark/training-monolingual.tokenized.shuffled/news.en-*-of-*)
+# DEVEL_FILE=/path/to/google/1-billion-word-language-modeling-benchmark/heldout-monolingual.tokenized.shuffled/news.en.heldout-00000-of-00050
+# EVAL_FILE=/path/to/google/1-billion-word-language-modeling-benchmark/heldout-monolingual.tokenized.shuffled/news.en.heldout-00001-of-00050
+# OUTPUT_DIR=/path/to/output/directory
+#
+source "${script_dir}/paths.sh"
+
+# Load common functions.
+source "${script_dir}/../common/functions.sh"
+source "${script_dir}/../common/configure-theano.sh"
+
+# Set training parameters.
+BATCH_SIZE=16
+VOCAB_MIN_COUNT=3
+OPTIMIZATION_METHOD=adagrad
+MAX_GRADIENT_NORM=0.1
+STOPPING_CRITERION=no-improvement
+VALIDATION_FREQ=4
+PATIENCE=0
+ARCHITECTURE_FILE="${arch_dir}/word-gcnn-8b-fast.arch"
+COST=cross-entropy
+LEARNING_RATE=0.1
+#DEBUG=1
+
+rm -f "${OUTPUT_DIR}/nnlm.h5"
+mv -f "${script_dir}/gcnn.log" "${script_dir}/gcnn.log~" 2>/dev/null || true
+train | tee "${script_dir}/gcnn.log"
+compute_perplexity | tee --append "${script_dir}/gcnn.log"
